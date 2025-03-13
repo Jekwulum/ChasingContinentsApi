806775930d3d4f1a4475044edb9479050c47bf52
Charles <charlesnwoye2@gmail.com>
2025-03-13 22:32:24 +0100

    optimise response to frontend

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import pytz
 import os
 import itertools
+import json
+import datetime
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from dotenv import load_dotenv
-from datetime import datetime
 from withstops import WithStops
 from directonly import DirectFlight
 
@@ -19,16 +20,26 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
+
+def serialize_datetime(obj):
+    if isinstance(obj, datetime.datetime):
+        return obj.isoformat()
+    elif isinstance(obj, datetime.timedelta):
+        return str(obj)
+    raise TypeError(f"Type not serializable: {obj}")
+
+
 @app.route("/api/health", methods=["GET"])
 def health_check():
     return jsonify({"status": "UP"})
+
 
 @app.route("/api/flights", methods=["GET"])
 def fetch_flights():
     start_origin = request.args.get("start_origin")
     departure_date = request.args.get("departure_date")
     departure_time = request.args.get("departure_time")
-    flight_type = request.args.get("flight_type", "direct") # direct or stops
+    flight_type = request.args.get("flight_type", "direct")  # direct or stops
 
     try:
         current_time = datetime.strptime(
@@ -97,10 +108,24 @@ def fetch_flights():
             valid_itineraries, key=lambda x: x[1]["total_travel_time"]
         )
 
-        print(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
-        print({best_sequence, best_itinerary})
+        print(
+            "::::::::::::::::::::::::::::::best sequence:::::::::::::::::::::::::::::::::::::::"
+        )
+        print(best_sequence)
 
-        return jsonify({"status": "SUCCESS", "data": {best_sequence, best_itinerary}})
+        print(
+            "::::::::::::::::::::::::::::::best itinerary:::::::::::::::::::::::::::::::::::::::"
+        )
+        print(best_itinerary)
+
+        best_itinerary = json.dumps(best_itinerary, default=serialize_datetime)
+
+        return jsonify(
+            {
+                "status": "SUCCESS",
+                "data": {"sequence": best_sequence, "itinerary": best_itinerary},
+            }
+        )
     else:
         return jsonify(
             {
@@ -108,7 +133,92 @@ def fetch_flights():
                 "message": "No valid itineraries were found across all sequences.",
             }
         )
+    
 
+@app.route("/api/tests", methods=["GET"])
+def test():
+    data = {
+        'flights': [
+            {
+                'airline': 'LA',
+                'flight_number': 'LA896',
+                'departure_time': datetime.datetime(2025, 3, 15, 20, 20, tzinfo=datetime.timezone.utc),
+                'arrival_time': datetime.datetime(2025, 3, 15, 23, 45, tzinfo=datetime.timezone.utc),
+                'origin': 'PUQ',
+                'destination': 'SCL',
+                'duration': datetime.timedelta(seconds=12300),
+                'cost': 79.1,
+                'layover': datetime.timedelta(seconds=10200),
+                'layover_iata': 'PUQ'
+            },
+            {
+                'airline': 'LA',
+                'flight_number': 'LA504',
+                'departure_time': datetime.datetime(2025, 3, 16, 9, 25, tzinfo=datetime.timezone.utc),
+                'arrival_time': datetime.datetime(2025, 3, 16, 16, 55, tzinfo=datetime.timezone.utc),
+                'origin': 'SCL',
+                'destination': 'MIA',
+                'duration': datetime.timedelta(seconds=30600),
+                'cost': 845.81,
+                'layover': datetime.timedelta(seconds=34800),
+                'layover_iata': 'SCL'
+            },
+            {
+                'airline': 'UX',
+                'flight_number': 'UX98',
+                'departure_time': datetime.datetime(2025, 3, 16, 22, 45, tzinfo=datetime.timezone.utc),
+                'arrival_time': datetime.datetime(2025, 3, 17, 12, 5, tzinfo=datetime.timezone.utc),
+                'origin': 'MIA',
+                'destination': 'MAD',
+                'duration': datetime.timedelta(seconds=30000),
+                'cost': 414.0,
+                'layover': datetime.timedelta(seconds=21000),
+                'layover_iata': 'MIA'
+            },
+            {
+                'airline': 'MS',
+                'flight_number': 'MS754',
+                'departure_time': datetime.datetime(2025, 3, 17, 14, 50, tzinfo=datetime.timezone.utc),
+                'arrival_time': datetime.datetime(2025, 3, 17, 20, 35, tzinfo=datetime.timezone.utc),
+                'origin': 'MAD',
+                'destination': 'CAI',
+                'duration': datetime.timedelta(seconds=17100),
+                'cost': 194.89,
+                'layover': datetime.timedelta(seconds=9900),
+                'layover_iata': 'MAD'
+            },
+            {
+                'airline': 'MS',
+                'flight_number': 'MS910',
+                'departure_time': datetime.datetime(2025, 3, 17, 22, 35, tzinfo=datetime.timezone.utc),
+                'arrival_time': datetime.datetime(2025, 3, 18, 3, 45, tzinfo=datetime.timezone.utc),
+                'origin': 'CAI',
+                'destination': 'DXB',
+                'duration': datetime.timedelta(seconds=11400),
+                'cost': 234.99,
+                'layover': datetime.timedelta(seconds=7200),
+                'layover_iata': 'CAI'
+            },
+            {
+                'airline': 'EK',
+                'flight_number': 'EK424',
+                'departure_time': datetime.datetime(2025, 3, 18, 9, 15, tzinfo=datetime.timezone.utc),
+                'arrival_time': datetime.datetime(2025, 3, 19, 0, 10, tzinfo=datetime.timezone.utc),
+                'origin': 'DXB',
+                'destination': 'PER',
+                'duration': datetime.timedelta(seconds=39300),
+                'cost': 1185.4,
+                'layover': datetime.timedelta(seconds=19800),
+                'layover_iata': 'DXB'
+            }
+        ],
+        'total_flight_duration': datetime.timedelta(days=1, seconds=54300),
+        'total_layover_duration': datetime.timedelta(days=1, seconds=16500),
+        'total_travel_time': datetime.timedelta(days=2, seconds=79800),
+        'total_cost': 2954.1899999999996
+    }
+    json_data = json.dumps(data, default=serialize_datetime)
+    return jsonify({"status": "SUCCESS", "data": json_data})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
