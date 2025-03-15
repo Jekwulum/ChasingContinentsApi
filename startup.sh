365ef5833810235f2ce39a9aeababd80ca9a0337
Charles <charlesnwoye2@gmail.com>
2025-03-15 13:21:51 +0100

    modify startup

--- startup.sh
+++ startup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Starting the application"
-python  -m  pip  install  --upgrade  pip
-pip  install  -r  requirements.txt
+python -m pip install --upgrade pip
+pip install -r requirements.txt
 
 echo "requirments installed"
-# python3 app.py
-gunicorn --bind=0.0.0.0 --timeout 0 app:app
+python3 app.py
+# gunicorn --bind=0.0.0.0 --timeout 0 app:app
+# gunicorn  -b  0.0.0.0:${WEBSITES_PORT:-8000} -w  4  --timeout  600  wsgi:app
