4585e90de0d680e80119a1cd10a6ccdd86dee678
Charles <charlesnwoye2@gmail.com>
2025-03-15 13:15:30 +0100

    modify startup

--- startup.sh
+++ startup.sh
@@ -4,4 +4,5 @@ python  -m  pip  install  --upgrade  pip
 pip  install  -r  requirements.txt
 
 echo "requirments installed"
-python3 app.py
+# python3 app.py
+gunicorn --bind=0.0.0.0 --timeout 0 app:app
